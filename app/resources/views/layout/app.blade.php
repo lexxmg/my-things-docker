1c15a6472aa04573f97f95e6856acd96f80b750d
lexxmg <lexx.mg@gmail.com>
2024-11-11 23:31:41 +0300

    css

--- app/resources/views/layout/app.blade.php
+++ app/resources/views/layout/app.blade.php
@@ -21,7 +21,9 @@
     <body>
         <div class="wrapper">
             <header class="header">
-                <h1 class="header__title">Мои вещи</h1>
+                <div class="header__left">
+                    <h1 class="header__title">Мои вещи</h1>
+                </div>
 
                 <nav class="nav-mob">
                     <ul class="nav-mob__list nav-mob-list">
@@ -34,27 +36,60 @@
                     </ul>
                 </nav>
 
-                <nav class="header-nav">
-                    <ul class="header-nav__list header-nav-list">
-                        <li class="header-nav-list__item">
-                            <a class="btn btn--things" href="#" @disabled(false)>1</a>
-                        </li>
-                        <li class="header-nav-list__item">
-                            <a class="btn btn--box" href="#" @disabled(false)>2</a>
-                        </li>
-                    </ul>
-                </nav>
+                <div class="header__center">
+                    <nav class="header-nav">
+                        <ul class="header-nav__list header-nav-list">
+                            <li class="header-nav-list__item">
+                                <a class="header-nav-list__link" href="#">Вещи</a>
+                            </li>
+                            <li class="header-nav-list__item">
+                                <a class="header-nav-list__link" href="#">Коробки</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+
+                <div class="header__right header-right">
+                    <span class="header-right__text">lexx@gmail.com</span>
+                    <span class="header-right__text">выход</span>
+                </div>
             </header>
 
-            <main class="main"></main>
+            <main class="main fixed-container">
+                <div style="height: 10000px;">
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur
+                        dipisicing elit. At suscipit dolorem ipsa ad maiores ipsam inventore sint itaque voluptatibus cumque beatae sequi optio id vel quia, numquam eveniet exercitationem modi!
+                        dipisicing elit. At suscipit dolorem ipsa ad maiores ipsam inventore sint itaque voluptatibus cumque beatae sequi optio id vel quia, numquam eveniet exercitationem modi!
+                        dipisicing elit. At suscipit dolorem ipsa ad maiores ipsam inventore sint itaque voluptatibus cumque beatae sequi optio id vel quia, numquam eveniet exercitationem modi!
+                        dipisicing elit. At suscipit dolorem ipsa ad maiores ipsam inventore sint itaque voluptatibus cumque beatae sequi optio id vel quia, numquam eveniet exercitationem modi!
+                        dipisicing elit. At suscipit dolorem ipsa ad maiores ipsam inventore sint itaque voluptatibus cumque beatae sequi optio id vel quia, numquam eveniet exercitationem modi!
+                        dipisicing elit. At suscipit dolorem ipsa ad maiores ipsam inventore sint itaque voluptatibus cumque beatae sequi optio id vel quia, numquam eveniet exercitationem modi!
+
+                        dipisicing elit. At suscipit dolorem ipsa ad maiores ipsam inventore sint itaque voluptatibus cumque beatae sequi optio id vel quia, numquam eveniet exercitationem modi!
+                        dipisicing elit. At suscipit dolorem ipsa ad maiores ipsam inventore sint itaque voluptatibus cumque beatae sequi optio id vel quia, numquam eveniet exercitationem modi!
+                        dipisicing elit. At suscipit dolorem ipsa ad maiores ipsam inventore sint itaque voluptatibus cumque beatae sequi optio id vel quia, numquam eveniet exercitationem modi!
+                        dipisicing elit. At suscipit dolorem ipsa ad maiores ipsam inventore sint itaque voluptatibus cumque beatae sequi optio id vel quia, numquam eveniet exercitationem modi!
+
+                    </p>
+                </div>
+            </main>
 
             <footer class="footer">
-                <span>fuutert</span>
                 <nav class="footer-nav">
                     <ul class="footer-nav__list footer-nav-list">
-                        <li class="footer-nav-list__item">1</li>
-                        <li class="footer-nav-list__item">2</li>
-                        <li class="footer-nav-list__item">3</li>
+                        <li class="footer-nav-list__item">
+                            <span class="footer-nav-list__text icon-home"></span>
+                            <a href="" class="footer-nav-list__link">Главная</a>
+                        </li>
+                        <li class="footer-nav-list__item">
+                            <span class="footer-nav-list__text icon-search"></span>
+                            <a href="" class="footer-nav-list__link">Поиск</a>
+                        </li>
+                        <li class="footer-nav-list__item">
+                            <span class="footer-nav-list__text icon-settingsfour-gearsalt"></span>
+                            <a href="" class="footer-nav-list__link">Настройки</a>
+                        </li>
                     </ul>
                 </nav>
             </footer>
